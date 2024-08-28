460e49602d1b94863d35406250f75a9383df18f7
IT24100368 <it24100368@my.sliit.lk>
2024-08-28 12:13:49 +0530

    Add to IT24100368Lab6Q2A.java to Q2A

---
+++ Q2/IT24100368Lab6Q2A.java
@@ -0,0 +1,15 @@
+public class IT24100368Lab6Q2A
+{
+	public static void main(String[] args)
+	{
+		
+		int number;
+
+		number=1;
+		while(number<=10)
+		{
+			System.out.print(number+  "  ");
+			number++;
+		}
+	}
+}
